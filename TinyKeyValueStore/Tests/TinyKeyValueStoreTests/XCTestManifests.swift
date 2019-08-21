12ec3b590a453863ff0602a06fa5f38c83df7cae
Roy Hsu <roy.hsu@tinyworld.cc>
2019-08-21 09:37:00 +0800

    minors.

--- TinyKeyValueStore/Tests/TinyKeyValueStoreTests/XCTestManifests.swift
+++ TinyKeyValueStore/Tests/TinyKeyValueStoreTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(CacheTests.allTests),
+        testCase(FieldTests.allTests),
         testCase(MemoryTests.allTests),
     ]
 }
