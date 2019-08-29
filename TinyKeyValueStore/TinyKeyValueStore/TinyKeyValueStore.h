88dc86f49fc0f8008de6dbebe2cd5af8e15cdc8a
Roy Hsu <roy.hsu@tinyworld.cc>
2019-08-29 16:03:18 +0800

    revert project settings to be compatible with older Xcode version.

---
+++ TinyKeyValueStore/TinyKeyValueStore/TinyKeyValueStore.h
@@ -0,0 +1,19 @@
+//
+//  TinyKeyValueStore.h
+//  TinyKeyValueStore
+//
+//  Created by Roy Hsu on 2019/8/29.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyKeyValueStore.
+FOUNDATION_EXPORT double TinyKeyValueStoreVersionNumber;
+
+//! Project version string for TinyKeyValueStore.
+FOUNDATION_EXPORT const unsigned char TinyKeyValueStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyKeyValueStore/PublicHeader.h>
+
+
